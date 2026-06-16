404850bbcb6493e69e590431ff65fa79a0249bc0
Lukas Böhm <lukas@bioinf.uni-leipzig.de>
2026-06-16 15:02:52 +0200

    fix install?

--- install.py
+++ install.py
@@ -1,20 +1,32 @@
+#!/usr/bin/python3
 import platform
 import os
 import tomllib
 from shutil import copyfile, copytree, ignore_patterns, copy, rmtree
+from pathlib import Path
 
 # source: https://github.com/typst/packages#local-packages
 
 
-def determine_data_dir() -> str:
-    if platform.system().lower() == "linux":
-        return os.path.join(os.environ["HOME"], ".local", "share")
-    elif platform.system().lower() == "windows":
-        return os.path.join(os.environ["APPDATA"])
-    elif platform.system().lower() == "darwin":
-        return os.path.join(os.environ["HOME"], "Library", "Application Support")
+def determine_data_dir() -> Path:
+    plat = platform.system().lower() 
+    if plat == "linux":
+        return Path(os.environ["HOME"]) / ".local" / "share"
+    elif plat == "windows":
+        return Path(os.environ["APPDATA"])
+    elif plat == "darwin":
+        return Path(os.environ["HOME"]) / "Library" / "Application Support"
     else:
         raise NotImplementedError("Unknown platform")
+
+
+def determine_template_dir() -> Path:
+    platform = platform.system().lower()
+    if platform == "linux":
+        template_path = Path(os.environ["HOME"]) / "Templates"
+        if template_path.exists():
+            return template_path
+    raise ValueError("no template path available")
 
 
 def copy_files(source_dir: str, target_dir: str) -> None:
@@ -47,18 +59,16 @@ def copy_files(source_dir: str, target_dir: str) -> None:
 
 
 if __name__ == "__main__":
-    name = ""
-    version = ""
     with open("typst.toml", "rb") as f:
         toml = tomllib.load(f)
         name = toml["package"]["name"]
         version = toml["package"]["version"]
 
     # copy files
-    target_dir = os.path.join(determine_data_dir(), "typst", "packages", "local", name, version)
+    target_dir = determine_data_dir() / "typst" / "packages" / "local" / name / version
     print("Copying files to", target_dir)
     copy_files(
         source_dir=os.getcwd(),
-        target_dir=target_dir
+        target_dir=str(target_dir)
     )
     print("Done.")
